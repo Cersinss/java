69dc1303a39ba36db5f66f6d1b6213aa55a5d52f
Cersinss <147008408+Cersinss@users.noreply.github.com>
2023-10-13 11:06:07 +0300

    Add files via upload

---
+++ razdel chet.js
@@ -0,0 +1,6 @@
+const chet = 500;
+const people = 3500 / chet;
+const chatvie = 500 / people;
+const obch_chet = 3500;
+
+console.log('С каждого по ' + chet + ' рублей, при общем счёте ' + obch_chet + ' рублей и чаевыми ' + chatvie + ' рублей');
