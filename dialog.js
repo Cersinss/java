835d7f9fc16680d92cb998425388fdf8f5a2ebdc
Cersinss <147008408+Cersinss@users.noreply.github.com>
2023-10-13 10:21:22 +0300

    Add files via upload

---
+++ dialog.js
@@ -0,0 +1,2 @@
+console.log('Jesse Pinkman: "Yo, Mr. White! I have got a surprise for you!"\nWalter White: "What is it?"\nJesse Pinkman: "It is a giant magnet!"\nWalter White: "What the hell are we going to do with a giant magnet?"');
+
